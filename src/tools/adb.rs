62a6b68e3f98010bd77590cbd3d1a60b134ecc94
Marley Reeves <marley@marlstar.com>
2025-10-14 08:35:10 +1000

    feat(adb): get installed path and different version numbers

--- src/tools/adb.rs
+++ src/tools/adb.rs
@@ -1,7 +1,10 @@
 use std::process::Command;
+use std::path::PathBuf;
 use regex::Regex;
 
-pub fn adb_version() -> Result<(usize, usize, usize), AdbError> {
+pub type AdbVersion = (usize, usize, usize);
+
+pub fn adb_version() -> Result<AdbInstall, AdbError> {
     let stdout = Command::new("adb")
         .arg("--version")
         .output()
@@ -10,8 +13,20 @@ pub fn adb_version() -> Result<(usize, usize, usize), AdbError> {
     let adb_version = String::from_utf8(stdout).unwrap();
     println!("stdout:\n{adb_version}");
 
-    let r = Regex::new("Android Debug Bridge version (?<v1>([0-9]+))\\.(?<v2>([0-9]+))\\.(?<v3>([0-9]+))").unwrap();
-    let capture = r.captures(&adb_version)
+    const SEMVER_REGEX: &str = "(?<v1>([0-9]+))\\.(?<v2>([0-9]+))\\.(?<v3>([0-9]+))";
+    let server_version_reg = Regex::new(&format!("Android Debug Bridge version {SEMVER_REGEX}")).unwrap();
+    let version_reg = Regex::new(&format!("Version {SEMVER_REGEX}-android-tools")).unwrap();
+    let path_reg = Regex::new("Installed as (?<path>(.*))").unwrap();
+
+    let capture = server_version_reg.captures(&adb_version)
+        .ok_or(AdbError::NotInstalled)?;
+    let server_version = (
+        capture["v1"].parse::<usize>().unwrap(),
+        capture["v2"].parse::<usize>().unwrap(),
+        capture["v3"].parse::<usize>().unwrap()
+    );
+
+    let capture = version_reg.captures(&adb_version)
         .ok_or(AdbError::NotInstalled)?;
     let version = (
         capture["v1"].parse::<usize>().unwrap(),
@@ -19,7 +34,19 @@ pub fn adb_version() -> Result<(usize, usize, usize), AdbError> {
         capture["v3"].parse::<usize>().unwrap()
     );
 
-    Ok(version)
+    println!("path time");
+    let capture = path_reg.captures(&adb_version)
+        .ok_or(AdbError::NotInstalled)?;
+    let path = PathBuf::from(&capture["path"]);
+
+    Ok(AdbInstall { version, server_version, path })
+}
+
+#[derive(Debug, Clone)]
+pub struct AdbInstall {
+    version: AdbVersion,
+    server_version: AdbVersion,
+    path: PathBuf,
 }
 
 #[derive(Debug)]
