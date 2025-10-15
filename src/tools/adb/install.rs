660e5243743782c65011a6d2cbb2846fe8d77922
Marley Reeves <marley@marlstar.com>
2025-10-15 11:13:41 +1000

    fix(adb): async archive extraction

--- src/tools/adb/install.rs
+++ src/tools/adb/install.rs
@@ -1,8 +1,10 @@
 use std::path::PathBuf;
-use std::sync::LazyLock;
 use std::io::Cursor;
+use std::sync::LazyLock;
+use async_zip::tokio::read::seek::ZipFileReader;
+use tokio::fs::File;
+use tokio_util::compat::FuturesAsyncReadCompatExt;
 use super::check_install::AdbInstall;
-
 
 #[cfg(windows)]
 const URL: &str = "https://dl.google.com/android/repository/platform-tools-latest-windows.zip";
@@ -27,8 +29,27 @@ pub async fn force() -> Result<(), InstallError> {
 
     tracing::info!("Downloaded ADB zip");
 
-    let mut archive = zip::ZipArchive::new(Cursor::new(response.bytes().await?))?;
-    archive.extract_unwrapped_root_dir(&*BINDIR, |path| path.starts_with("platform-tools"))?;
+    let bytes = response.bytes().await?;
+    let cursor = Cursor::new(bytes);
+    let mut archive = ZipFileReader::with_tokio(cursor).await?;
+
+    let entries = archive.file().entries().to_vec();
+    for (i, entry) in entries.into_iter().enumerate() {
+        let name = entry.filename().as_str()?.replace("platform-tools/", "");
+        tracing::debug!("Extracting {name}");
+        let path = BINDIR.join(name);
+        tokio::fs::create_dir_all(&path.parent().unwrap()).await?;
+        let mut reader = archive.reader_with_entry(i).await?.compat();
+        let mut file = File::create(&path).await?;
+        tokio::io::copy(&mut reader, &mut file).await?;
+        #[cfg(unix)] {
+            use std::fs::Permissions;
+            use std::os::unix::fs::PermissionsExt;
+            let perms = entry.unix_permissions().unwrap();
+            file.set_permissions(Permissions::from_mode(perms as u32)).await?;
+        }
+    }
+    // archive.extract_unwrapped_root_dir(&*BINDIR, |path| path.starts_with("platform-tools"))?;
 
     tracing::info!("Extracted ADB zip");
     tracing::info!("ADB installed successfully");
@@ -53,5 +74,5 @@ pub enum InstallError {
     #[error("Filesystem error: {0:?}")]
     Filesystem(std::io::Error),
     #[error("Zip archive error: {0:?}")]
-    Archive(zip::result::ZipError),
+    Archive(async_zip::error::ZipError),
 }
